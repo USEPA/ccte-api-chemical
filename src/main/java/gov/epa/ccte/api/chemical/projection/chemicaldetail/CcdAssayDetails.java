892055a155faf3ad39b964592b59c38f1221da8a
SrikanthDeevi <deevi.srikanth@epa.gov>
2025-07-25 13:47:49 -0400

    CE-4860 updated the base projection

--- src/main/java/gov/epa/ccte/api/chemical/projection/chemicaldetail/CcdAssayDetails.java
+++ src/main/java/gov/epa/ccte/api/chemical/projection/chemicaldetail/CcdAssayDetails.java
@@ -1,10 +1,7 @@
 package gov.epa.ccte.api.chemical.projection.chemicaldetail;
 
-public interface CcdAssayDetails extends ChemicalDetailBase{
+public interface CcdAssayDetails extends CcdChemicalDetails{
 	
-	String getStereo();
-	
-	Integer getActiveAssays();
 
 	default String getHitc() {
 		if (getActiveAssays() != null && getActiveAssays() > 0) {
