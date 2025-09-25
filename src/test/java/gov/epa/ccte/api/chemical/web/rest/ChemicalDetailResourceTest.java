8321e04109e3c016d3f27811af3055d06ba284f6
BDIRKS <BDIRKS@host.docker.internal>
2025-09-25 09:00:01 -0400

    Fix JUnit 4 discrepancy #250

--- src/test/java/gov/epa/ccte/api/chemical/web/rest/ChemicalDetailResourceTest.java
+++ src/test/java/gov/epa/ccte/api/chemical/web/rest/ChemicalDetailResourceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 //This will test REST end-points in the ChemicalDetailResource.java using WebMvcTest and MockitoBean
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.projection.ProjectionFactory;
@@ -32,6 +34,7 @@ import java.util.*;
 
 @ActiveProfiles("test")
 @WebMvcTest(ChemicalDetailResource.class)
+@RunWith(MockitoJUnitRunner.class)
 public class ChemicalDetailResourceTest {
 	
 	@Autowired
