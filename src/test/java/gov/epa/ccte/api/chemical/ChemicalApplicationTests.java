f91c0003a03dc8542e597b5886dda1df5c158cf7
SrikanthDeevi <deevi.srikanth@epa.gov>
2026-07-09 16:53:17 -0400

    CE-5130- Added contextLoads test

--- src/test/java/gov/epa/ccte/api/chemical/ChemicalApplicationTests.java
+++ src/test/java/gov/epa/ccte/api/chemical/ChemicalApplicationTests.java
@@ -1,12 +1,29 @@
 package gov.epa.ccte.api.chemical;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import gov.epa.ccte.api.chemical.repository.ChemicalDetailRepository;
+import gov.epa.ccte.api.chemical.service.ChemicalDetailService;
+
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ChemicalApplicationTests {
+	
+	@MockitoBean
+	ChemicalDetailService ChemicalDetailService;
+	
+	@MockitoBean
+	ChemicalDetailRepository chemicalDetailRepository;
+
+	@Test
+	void contextLoads() {
+	}
 
 
 }
